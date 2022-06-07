3dbe5e1a3604c64e2f0fb1659ca432df6aa96255
sanjoy.wong <sanjoy.wong@gmail.com>
2022-06-07 14:45:30 +0200

    ajoute nav

---
+++ includes/main.php
@@ -0,0 +1,6 @@
+<<?php 
+require_once './includes/nav.php';
+?>
+<main>
+    <h1>Test</h1>
+</main>
---
+++ includes/nav.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="../index.php" target="_blank" rel="noopener noreferrer">Accueil</a>
+            </li>
+        </ul>
+    </nav>
+</header>

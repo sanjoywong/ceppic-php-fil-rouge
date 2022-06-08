d29c00e4b724f7fac9e10b4cc50802d0d16abfba
sanjoy.wong <sanjoy.wong@gmail.com>
2022-06-08 09:21:30 +0200

    ajoute inscription login

--- includes/nav.php
+++ includes/nav.php
@@ -2,8 +2,11 @@
     <nav>
         <ul style="list-style-type: none;">
             <li>
-                <a href="index.php" target="_blank" rel="noopener noreferrer" >Accueil</a>
+                <a href="index.php?page=accueil" target="_blank" rel="noopener noreferrer" >Accueil</a>
             </li>
+            <li><a href="index.php?page=contact">contact</a></li>
+            <li><a href="index.php?page=inscription">inscription</a></li>
+            <li><a href="index.php?page=login">login</a></li>
         </ul>
     </nav>
 </header>

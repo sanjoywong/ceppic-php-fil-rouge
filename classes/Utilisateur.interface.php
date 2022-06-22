9e9649e94fbfd9eb2cd6edbf7a25753929da79dc
sanjoy.wong <sanjoy.wong@gmail.com>
2022-06-22 11:24:39 +0200

    ajoute class utilisateur.interface

---
+++ classes/Utilisateur.interface.php
@@ -0,0 +1,17 @@
+<?php
+
+interface Utilisateur
+{
+
+    public function inscrireUtilisateur();
+    public function modifierUtilisateur();
+    public function supprimerUtilisateur();
+    public function connecterUtilisateur();
+    public function deconnecterUtilisateur();
+}
+
+
+
+
+
+?>
